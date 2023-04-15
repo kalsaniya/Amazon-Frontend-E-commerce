b04f0a92fb186367b7d1b78ffd5b8e40cb177a1d
Gurmeet Singh <gurmeetsingh6175@gmail.com>
2023-04-15 11:37:52 +0530

    Update

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+const About = () => {
+  return (
+    <>
+      <Meta title={"About Us"} />
+      <BreadCrumb title="About Us" />
+    </>
+  );
+};
+export default About;
